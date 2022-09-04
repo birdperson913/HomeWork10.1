1650fe654a72dfe331cd1e05c9411615b1ec2630
birdperson913 <81969845+birdperson913@users.noreply.github.com>
2022-09-04 19:12:44 +0300

    remake class RadioClass add new constructor remace RadioTest new2

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -53,7 +53,7 @@ this.amountStation=10;
     public void prev() {
         if (station > 0) {
             station = station - 1;
-        } else station = amountStation;
+        } else station = amountStation-1;
 
 
     }
